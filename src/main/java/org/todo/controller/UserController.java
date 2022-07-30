520fc152fa2c542aa0a0428fabd2ed231e6c4ff9
GrowMyself <deae670@daum.net>
2022-07-30 16:12:15 +0900

    Login implemented Ok, but develop is needed Ahead

--- src/main/java/org/todo/controller/UserController.java
+++ src/main/java/org/todo/controller/UserController.java
@@ -7,23 +7,31 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/member")
 public class UserController {
 
+    // 회원정보 등록
     @PostMapping
     public ResponseEntity postMember() {
         return null;
     }
 
+    // 회원 정보 수정
     @PatchMapping
     public ResponseEntity patchMember() {
         return null;
     }
+
+    // 회원 1명 조회
     @GetMapping("/{userId}")
     public ResponseEntity getMember(@PathVariable Long userId) {
         return null;
     }
+
+    // 회원 전체 조회
     @GetMapping
     public ResponseEntity getMembers() {
         return null;
     }
+
+    // 회원 탈퇴
     @DeleteMapping
     public ResponseEntity deleteMember() {
         return null;
